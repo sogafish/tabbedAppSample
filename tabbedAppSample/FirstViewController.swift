c2136e069930172027cb62a55ad3b2a55538ed86
sogafish <k.sogafish@gmail.com>
2019-11-04 20:48:24 +0900

    :tada:

---
+++ tabbedAppSample/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  tabbedAppSample
+//
+//  Created by k.soga on 2019/11/04.
+//  Copyright © 2019 k.soga. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
