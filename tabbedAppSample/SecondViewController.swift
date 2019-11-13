0844be28430a157b98abfbf7c039d287bdf884ac
sogafish <k.sogafish@gmail.com>
2019-11-14 02:33:00 +0900

    :sparkles: Add Actionsheet To Secound

--- tabbedAppSample/SecondViewController.swift
+++ tabbedAppSample/SecondViewController.swift
@@ -9,12 +9,54 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    @IBAction func ShowActionController(_ sender: Any) {
+        let actionSheet = UIAlertController(title: "ActionSheetTitle", message: "ActionSeetMessage", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(
+            title: "ButtonA",
+            style: .default,
+            handler: {(action) -> Void in
+                print(action.title!)
+                print("AAA")
+            }
+        ))
+        
+        actionSheet.addAction(UIAlertAction(
+            title: "ButtonB",
+            style: .default,
+            handler: {(action) -> Void in
+                print(action.title!)
+                print("BBB")
+            }
+        ))
+        
+        actionSheet.addAction(UIAlertAction(
+            title: "Cancel",
+            style: .cancel,
+            handler: nil
+        ))
+        
+        actionSheet.addAction(UIAlertAction(
+            title: "Destructive",
+            style: .destructive,
+            handler: {(action) -> Void in
+                print(action.title!)
+                print("DESTRUCTIVE")
+            }
+        ))
+        
+        present(
+            actionSheet,
+            animated: true,
+            completion: {
+              print("COMPLETION !!!")
+            }
+        )
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
