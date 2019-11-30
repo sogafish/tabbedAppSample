b93ec020f960cccd58cd957b7a0fa751887b4dee
sogafish <k.sogafish@gmail.com>
2019-11-30 17:00:57 +0900

    :sparkles: Add transitionFlipFromLeft AtoView To 2

--- tabbedAppSample/SecondViewController.swift
+++ tabbedAppSample/SecondViewController.swift
@@ -55,8 +55,44 @@ class SecondViewController: UIViewController {
         )
     }
     
+    @IBOutlet weak var baseView: UIView!
+    
+    var fromView: UIView!
+    var toView: UIView!
+
+    @IBAction func showInfo(_ sender: Any) {
+        UIView.transition(
+            from: fromView,
+            to: toView,
+            duration: 1,
+            options: .transitionFlipFromLeft,
+            completion: {_ in
+                let tmpView = self.fromView
+                self.fromView = self.toView
+                self.toView = tmpView
+            }
+        )
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let atoView = UIImageView(image: UIImage(named: "ato"))
+        
+        atoView.center = CGPoint(x: baseView.bounds.midX, y: baseView.bounds.midY)
+        
+        let atoViewR = UILabel()
+        atoViewR.text = "阿藤 快（あとう かい、1946年11月14日 - 2015年11月14日）は、日本の俳優・タレントである。旧芸名は「阿藤 海」[2][1]、本名は阿藤 公一（あとう こういち）。"
+        
+        atoViewR.frame = CGRect(x: 20, y: 20, width: 140, height: 21)
+        atoViewR.backgroundColor = UIColor.gray
+        atoViewR.textColor = UIColor.white
+        
+        fromView = atoView
+        toView = atoViewR
+        
+        baseView.addSubview(fromView)
+        baseView.clipsToBounds = true
     }
 }
+
 
